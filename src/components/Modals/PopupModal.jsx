1e2ada36671c706e41623d8e5c9a4beea9b0e377
Vamshi Krishna <vamshi.krishna@live.in>
2025-06-06 20:13:07 +0530

    Beautify

--- src/components/Modals/PopupModal.jsx
+++ src/components/Modals/PopupModal.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 
-const PopupModal = ({
-  title,
-  description,
-  nextDate,
-  setShowFutureGoatPopup,
-}) => {
+const PopupModal = ({ title, description, nextDate, setShowFutureGoatPopup }) => {
   return (
     <div
       id="small-modal"
@@ -18,29 +13,15 @@ const PopupModal = ({
         <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
           {/* <!-- Modal header --> */}
           <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
-            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-              {title}
-            </h3>
+            <h3 className="text-xl font-medium text-gray-900 dark:text-white">{title}</h3>
             <button
               onClick={() => setShowFutureGoatPopup(false)}
               type="button"
               className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-hide="small-modal"
             >
-              <svg
-                className="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
+              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
               </svg>
               <span className="sr-only">Close modal</span>
             </button>
