223777b6a7db25610b1faae17ed9df0a1a3c81df
Vamshi Krishna <vamshi.krishna@live.in>
2025-07-05 09:55:52 +0530

    Update list of pincodes

--- src/staticValues.js
+++ src/staticValues.js
@@ -1,1 +1,1 @@
-module.exports = { pincodes: [502001], cityMap: { 533016: "Aratlakatta", 502001: "Sangareddy", "": "" } };
+module.exports = { pincodes: [502001, 502295, 502285], cityMap: { 502001: "Sangareddy", 502295: "Malkapur", 502285: "Kandi" } };
