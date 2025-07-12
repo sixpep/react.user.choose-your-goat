e8e2a09e91b312c3eccba08a5740b596c28b730f
Vamshi Krishna <vamshi.krishna@live.in>
2025-07-12 07:38:52 +0530

    Added few pincodes

--- src/staticValues.js
+++ src/staticValues.js
@@ -1,9 +1,11 @@
 module.exports = {
-  pincodes: ["502001", "502295", "502285"],
+  pincodes: ["502001", "502295", "502285", "502319", "500032"],
   cityMap: {
     502001: "Sangareddy",
     502295: "Malkapur",
     502285: "Kandi",
+    502319: "Patancheru",
+    500032: "Gachibowli",
   },
   chickenRestrictedAt: [],
   muttonRestrictedAt: [],
